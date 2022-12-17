0118e7723f513129bc9d8d04eb35ebd2461f5967
FredECI <fredeci@poli.ufrj.br>
2022-12-17 12:46:17 -0300

    adjusted hmm_class to return score in get_score

--- HMM_Model.py
+++ HMM_Model.py
@@ -1,8 +1,8 @@
 import os
-import soundfile as sf
-from pydub import AudioSegment
+# import soundfile as sf
+# from pydub import AudioSegment
 import numpy as np
-from scipy.io import wavfile 
+# from scipy.io import wavfile 
 from hmmlearn import hmm
 import librosa
 from librosa.feature import mfcc
@@ -22,29 +22,12 @@ class HMMTrainer(object):
         else:
             raise TypeError('Invalid model type')
 
-    # X is a 2D numpy array where each row is 13D
-    def train(self, X):
-        np.seterr(all='ignore')
-        self.models.append(self.model.fit(X))
-
-    # Run the model on input data
-    def get_score(self, input_data):
-        return self.model.score(input_data)
-    
-
-    def Training(input_folder):
-        hmm_models = []
-        for dirname in os.listdir(input_folder):
-            if dirname == '.ipynb_checkpoints':
-                continue
-            subfolder = os.path.join(input_folder, dirname)
-            if not os.path.isdir(subfolder): 
-                continue
-        label = subfolder[subfolder.rfind('/') + 1:]
+    def get_score(self, reference_folder, input_audio):
+        label = reference_folder[reference_folder.rfind('/') + 1:]
         X = np.array([])
         y_words = []
-        for filename in [x for x in os.listdir(subfolder) if x.endswith('.wav')][:-1]:
-            filepath = os.path.join(subfolder, filename)
+        for filename in [x for x in os.listdir(reference_folder) if x.endswith('.wav')][:-1]:
+            filepath = os.path.join(reference_folder, filename)
             sampling_freq, audio = librosa.load(filepath)            
             mfcc_features = mfcc(sampling_freq, audio)
             if len(X) == 0:
@@ -52,37 +35,28 @@ class HMMTrainer(object):
             else:
                 X = np.append(X, mfcc_features[:,:15], axis=0)            
             y_words.append(label)
-        print('X.shape =', X.shape)
-        hmm_trainer = HMMTrainer()
-        hmm_trainer.train(X)
-        hmm_models.append((hmm_trainer, label))
-        hmm_trainer = None
+        self.models = []
+        self._train(X)
+        score = self._compare_audios(input_audio)
 
-    def prediction(hmm_models,input_files):
-        for input_file in input_files:
-            sampling_freq, audio = librosa.load(input_file)
+        return score
 
-            # Extract MFCC features
-            mfcc_features = mfcc(sampling_freq, audio)
-            mfcc_features=mfcc_features[:,:15]
+    # X is a 2D numpy array where each row is 13D
+    def _train(self, X):
+        np.seterr(all='ignore')
+        self.model = self.model.fit(X)
 
-            scores=[]
-            for item in hmm_models:
-                hmm_model, label = item
-                score = hmm_model.get_score(mfcc_features)
-                scores.append(score)
-            index=np.array(scores).argmax()
+    def _compare_audios(self, input_file):
+        sampling_freq, audio = librosa.load(input_file)
 
-            print("\nTrue:", input_file)
-            print("Predicted:", hmm_models[index][1]) 
+        # Extract MFCC features
+        mfcc_features = mfcc(sampling_freq, audio)
+        mfcc_features=mfcc_features[:,:15]
+
+        score = self.model.score(mfcc_features)
+        return score
 
 
-    def DeleteEndpoints(audio,sr):
-        clips = librosa.effects.split(audio, top_db=15)
-        print(clips)
-        wav_data = []
-        for c in clips:
-            print(c)
-            data = audio[c[0]: c[1]]
-            wav_data.extend(data)
-        sf.write(f'5s.wav', wav_data, sr)
+# hmm_class = HMMTrainer()
+# score = hmm_class.get_score(r"audios\orange", r"audios\orange15.wav")
+# print(score)
